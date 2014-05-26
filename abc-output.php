42707b60718d221ac7f6d7c1c49b92ee8dd2a9a5
darkwhispering <darkwhispering@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-26 20:10:14 +0000

    prefixed core files to minimize conflicts with other plugins
    git-svn-id: https://plugins.svn.wordpress.org/advanced-browser-check/tags/3.0.1@921283 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ abc-output.php
@@ -0,0 +1,194 @@
+<?php
+// ========================
+// = Output			 	  =
+// ========================
+class ABC_Output extends ABC_Core {
+
+	public function __construct()
+	{
+
+		parent::__construct();
+
+		// Do the magic!
+		$this->html();
+
+	}
+
+	/**
+	* Check if we should display the popup
+	**/
+	private function html()
+	{
+
+		$user_browser 	= $this->get_browser();
+		$abc_options 	= $this->default_setting_values();
+		$check_browsers = $abc_options['check_browser'];
+		$show_browsers 	= $abc_options['show_browser'];
+		$title 			= $abc_options['title'];
+		$message 		= $abc_options['msg'];
+		$hide 			= $abc_options['hide'];
+		$debug			= $abc_options['debug'];
+
+		if ($debug === 'on' && current_user_can('level_10')) {
+
+			$old_ie = ($this->get_short_name($user_browser->Browser) === 'ie' && $user_browser->MajorVer < '8') ? 'old-ie' : '';
+			$this->build_html($title, $message, $show_browsers, $hide, $old_ie, $user_browser, $debug);
+
+		} else {
+
+			foreach($check_browsers as $browser => $version) {
+
+				if (
+					$this->get_short_name($user_browser->Browser) === $browser
+					&& $user_browser->MajorVer <= $version
+					&& $user_browser->Platform != 'Android'
+					&& $user_browser->Platform != 'iOS'
+					&& $user_browser->Platform != 'BB10')
+				{
+
+					$old_ie = ($this->get_short_name($user_browser->Browser) === 'ie' && $user_browser->MajorVer < '8') ? 'old-ie' : '';
+					$this->build_html($title, $message, $show_browsers, $hide, $old_ie, $user_browser);
+
+				}
+
+			}
+
+		}
+
+	}
+
+	/**
+	* Build up the HTML for the popup
+	**/
+	private function build_html($title = NULL, $msg = NULL, $show_browsers = array(), $hide = NULL, $old_ie = '', $user_browser = array(), $debug = false)
+	{
+
+		if ($debug) {
+
+			$debug_html = '<ul class="adv_browser_check_debug">';
+
+			foreach ($user_browser as $key => $value) {
+
+				$debug_html .= '<li><strong>'.$key.'</strong>: '.$value.'</li>';
+
+			}
+
+			$debug_html .= '</ul>';
+
+		}
+
+		$html = '<div class="adv_browser_check '.$old_ie.'">';
+
+			if ($debug) { $html .= $debug_html; }
+
+			$html .= '<div class="adv_browser_check_msg '.$old_ie.'">';
+				$html .= '<h1>'. $title .'</h2>';
+				$html .= nl2br($msg);
+			$html .= '</div>';
+			$html .= '<ul class="adv_browser_check_icons '.$old_ie.'">';
+
+				foreach($show_browsers as $browser => $link) {
+
+					if($link) {
+						
+						$html .= '<li>';
+							$html .= '<a href="'. $link .'" class="'. $browser .'" target="_blank">';
+								$html .= '<img src="'. plugins_url('/img/'. $browser .'-64x64.png', __FILE__) .'" alt="'. $browser .'">';
+								$html .= $this->get_full_name($browser);
+							$html .= '</a>';
+						$html .= '</li>';
+					}
+
+				}
+
+			$html .= '</ul>';
+
+			if($hide) {
+
+				$html .= '<a href="#" class="abc-hide"></a>';
+
+			}
+
+		$html .= '</div>';
+
+		echo $html;
+
+	}
+
+	/**
+	* Return short browser name based on the full name
+	**/
+	private function get_short_name($browser)
+	{
+
+		switch($browser) {
+
+			case 'Firefox':
+				$short = 'ff';
+				break;
+
+			case 'Chrome':
+				$short = 'chrome';
+				break;
+
+			case 'Safari':
+				$short = 'safari';
+				break;
+
+			case 'Opera':
+				$short = 'opera';
+				break;
+
+			case 'IE':
+				$short = 'ie';
+				break;
+
+			default:
+				$short = 'other';
+				break;
+
+		}
+
+		return $short;
+
+	}
+
+	/**
+	* Return full browser name based on the short name
+	**/
+	private function get_full_name($short)
+	{
+
+		switch($short) {
+
+			case 'ff':
+				$browser = 'Firefox';
+				break;
+
+			case 'chrome':
+				$browser = 'Chrome';
+				break;
+
+			case 'safari':
+				$browser = 'Safari';
+				break;
+
+			case 'opera':
+				$browser = 'Opera';
+				break;
+
+			case 'ie':
+				$browser = 'Internet Explorer';
+				break;
+
+			default:
+				$browser = 'other';
+				break;
+
+		}
+
+		return $browser;
+
+	}
+
+}
---
+++ abc-settings-page.php
@@ -0,0 +1,189 @@
+<?php
+// ========================
+// = Plugin settings page =
+// ========================
+
+class ABC_Settings extends ABC_Core {
+
+    public function __construct()
+    {
+
+        parent::__construct();
+
+    }
+
+	function settings_panel()
+    {
+		if (!current_user_can('manage_options')) {
+			wp_die(__('You do not have sufficient permissions to access this page.'));
+		}
+
+		$abc = $this->default_setting_values();
+
+	?>
+		<div class="wrap abc-settings">
+			<h2><?php _e('Advanced Browser Check'); ?></h2>
+
+			<form method="post" action="options.php">
+                <?php wp_nonce_field('update-options'); ?>
+
+				<table class="form-table abc-settings">
+                    <tbody>
+                        <tr valign="top">
+                            <th scope="row">
+                            	<label for="abc_title"><?php  _e('Title'); ?></label>
+                            </th>
+                            <td>
+								<input type="text" class="large-text" id="abc_title" name="abc_title" value="<?php echo $abc['title']; ?>">                
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                            	<label for="abc_message"><?php _e('Message'); ?></label>
+                            </th>
+                            <td>
+								<textarea class="large-text" id="abc_message" name="abc_message" cols="50" rows="8"><?php echo $abc['msg']; ?></textarea>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                            	<?php _e('Let user hide the popup'); ?>
+                            	<p class="description"><?php _e('Let the user hide the popup and use your site. The popup will be hidden for 24h only, this is set by a cookie'); ?></p>
+                            </th>
+                            <td>
+                            	<label for="hide">
+									<input type="checkbox" id="abc_hide" name="abc_hide" value="yes" <?php echo !empty($abc['hide']) ? 'checked="checked"' : '' ?> /> <?php _e('Yes'); ?>
+								</label>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                            	<?php _e('Choose browsers to link'); ?>
+                            	<p class="description"><?php _e('This is the browsers that you can display a link to and tell ypur visitor to use'); ?></p>
+                            </th>
+                            <td>
+                            	<ul>
+                            		<li>
+										<label for="ie">
+											<input type="checkbox" id="ie" name="abc_show[ie]" value="http://www.microsoft.com/windows/internet-explorer/worldwide-sites.aspx" <?php echo !empty($abc['show_browser']['ie']) ? 'checked="checked"' : '' ?> /> <?php _e('Internet Explorer'); ?>
+										</label>
+									</li>
+									<li>
+										<label for="ff">
+											<input type="checkbox" id="ff" name="abc_show[ff]" value="http://www.mozilla.com/en-US/firefox/all.html" <?php echo !empty($abc['show_browser']['ff']) ? 'checked="checked"' : '' ?> /> <?php _e('Firefox'); ?>
+										</label>
+									</li>
+									<li>
+										<label for="safari">
+											<input type="checkbox" id="safari" name="abc_show[safari]" value="http://www.apple.com/safari/download/" <?php echo !empty($abc['show_browser']['safari']) ? 'checked="checked"' : '' ?> /> <?php _e('Safari'); ?>
+										</label>
+									</li>
+									<li>
+										<label for="opera">
+											<input type="checkbox" id="opera" name="abc_show[opera]" value="http://www.opera.com/download/" <?php echo !empty($abc['show_browser']['opera']) ? 'checked="checked"' : '' ?> /> <?php _e('Opera'); ?>
+										</label>
+									</li>
+									<li>
+										<label for="chrome">
+											<input type="checkbox" id="chrome" name="abc_show[chrome]" value="https://www.google.com/chrome" <?php echo !empty($abc['show_browser']['chrome']) ? 'checked="checked"' : '' ?> /> <?php _e('Chrome'); ?>
+										</label>
+									</li>
+								</ul>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                            	<?php _e('Browsers and versions to check'); ?>
+                            </th>
+                            <td>
+                        		<ul>
+	                            	<?php $browsers = $this->default_browsers(); ?>
+									<?php $browser_selects = ''; ?>
+									<?php foreach($browsers as $key => $browser) : ?>
+										<li>
+											<?php $browser_selects .= $key.','; ?>
+											<select name="abc_check[<?php echo $key; ?>]">
+												<?php
+													switch($key) {
+														case $key == 'ff' :
+															$browser_name = 'Firefox';
+														break;
+														case $key == 'ie' :
+															$browser_name = 'Internet Explorer';
+														break;
+														case $key == 'safari' :
+															$browser_name = 'Safari';
+														break;
+														case $key == 'opera' :
+															$browser_name = 'Opera';
+														break;
+														case $key == 'chrome' :
+															$browser_name = 'Chrome';
+														break;
+													}
+												?>
+												<?php foreach($browser as $b) : ?>
+													<?php if ($b == '0') : ?>
+														<option value="<?php echo $b; ?>" <?php echo $abc['check_browser'][$key] == $b ? 'selected="selected"' : '' ?>>
+															<?php _e('Do not block ').$browser_name; ?>
+														</option>
+													<?php else : ?>
+														<option value="<?php echo $b; ?>" <?php echo $abc['check_browser'][$key] == $b ? 'selected="selected"' : '' ?>>
+															<?php echo $browser_name .' '. $b; ?> <?php _e(' or lower'); ?>
+														</option>
+													<?php endif; ?>
+												<?php endforeach; ?>
+											</select>
+										</li>
+									<?php endforeach; ?>
+								</ul>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                                <?php _e('Debug'); ?>
+                                <p class="description"><?php _e('Having problems. Activate debug and see what information the plugin detects about your browser. Debug is only visible for administrators!'); ?></p>
+                            </th>
+                            <td>
+                                <?php
+                                    $debug_on = $debug_off = null;
+
+                                    if (!empty($abc['debug'])) {
+                                        $debug_on = $abc['debug'] === 'on' ? 'checked="checked"' : '';
+                                        $debug_off = $abc['debug'] === 'off' ? 'checked="checked"' : '';
+                                    }
+                                ?>
+
+                                <label for="hide">
+                                    <input type="radio" id="abc_debug" name="abc_debug" value="on" <?php echo $debug_on; ?> /> <?php _e('On'); ?>
+                                    <br/>
+                                    <input type="radio" id="abc_debug" name="abc_debug" value="off" <?php echo $debug_off; ?> /> <?php _e('Off'); ?>
+                                </label>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+				<div class="form-row">
+					<input type="hidden" name="action" value="update" />
+					<input type="hidden" name="page_options" value="abc_title,abc_message,abc_hide,abc_show,abc_check,abc_debug" />
+					<input type="submit" name="submit" value="Save" class="button-primary save" />
+				</div>
+			</form>
+		</div>
+	<?php
+	}
+}
+
+function abc_settings()
+{
+	$settings_panel = new ABC_Settings;
+	return $settings_panel->settings_panel();
+}
+
+function abc_adminmenu()
+{
+	add_submenu_page('options-general.php', 'Advanced Browser Check', 'Advanced Browser Check', 'manage_options', 'advanced-browser-check', 'abc_settings');
+}
+
+add_action('admin_menu','abc_adminmenu');
+?>
